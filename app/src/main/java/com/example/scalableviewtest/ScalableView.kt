8f757acb74f8604ff1a0892d42b4748f75810557
Youssef Elerian <49160571+youssefelerian@users.noreply.github.com>
2024-07-08 00:04:52 +0300

    final

--- app/src/main/java/com/example/scalableviewtest/ScalableView.kt
+++ app/src/main/java/com/example/scalableviewtest/ScalableView.kt
@@ -28,6 +28,7 @@ class ScalableView @JvmOverloads constructor(
 
     private val recyclerViewTop = resources.getDimension(R.dimen.RecyclerViewTop).toInt()
     private val recyclerViewBottom = resources.getDimension(R.dimen.RecyclerViewBottom).toInt()
+    private var recyclerViewHeight = 0
 
     private val bottomSheetBehavior: BottomSheetBehavior<FrameLayout>? by lazy {
         binding?.bottomSheet?.let { BottomSheetBehavior.from(it) }
@@ -41,8 +42,10 @@ class ScalableView @JvmOverloads constructor(
             override fun onSlide(bottomSheet: View, slideOffset: Float) {
                 binding?.let {
                     val params = it.rv.layoutParams
-                    params.height =
-                        (it.parent.height) - (bottomSheet.height * slideOffset).toInt() - recyclerViewTop - recyclerViewBottom
+                    val scale = 1f - slideOffset
+                    if (scale > 0.2f)
+                        params.height = (recyclerViewHeight * scale).toInt()
+                    //  (it.parent.height) - (bottomSheet.height * slideOffset).toInt() - recyclerViewTop - recyclerViewBottom
                     it.rv.layoutParams = params
                 }
 
@@ -52,20 +55,22 @@ class ScalableView @JvmOverloads constructor(
 
     }
 
-    /* private fun setupBottomSheetInitialState(recyclerView: RecyclerView) = binding?.run {
+    private fun setupBottomSheetInitialState(recyclerView: RecyclerView) = binding?.run {
         recyclerView.viewTreeObserver.addOnGlobalLayoutListener(object :
             ViewTreeObserver.OnGlobalLayoutListener {
             override fun onGlobalLayout() {
-               ((recyclerView.rootView.height.toFloat() - recyclerView.layoutParams.height.toFloat()) / recyclerView.rootView.height.toFloat()).also { expandRatio ->
-                    bottomSheetBehavior?.halfExpandedRatio =
-                        if (expandRatio <= 0F || expandRatio >= 1) 0.1F else expandRatio
-                }
-                bottomSheetBehavior?.state = STATE_HALF_EXPANDED
-                recyclerView.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                /*((recyclerView.rootView.height.toFloat() - recyclerView.layoutParams.height.toFloat()) / recyclerView.rootView.height.toFloat()).also { expandRatio ->
+                     bottomSheetBehavior?.halfExpandedRatio =
+                         if (expandRatio <= 0F || expandRatio >= 1) 0.1F else expandRatio
+                 }
+                 bottomSheetBehavior?.state = STATE_HALF_EXPANDED*/
+                recyclerViewHeight = recyclerView.height
+                if (recyclerViewHeight > 0)
+                    recyclerView.viewTreeObserver.removeOnGlobalLayoutListener(this)
 
             }
         })
-    }*/
+    }
 
     fun addContent(content: Int) = binding?.bottomSheet?.run {
         addView(LayoutInflater.from(context).inflate(content, null))
@@ -75,7 +80,7 @@ class ScalableView @JvmOverloads constructor(
         lm = BottomStackLayoutManager(it)
         it.adapter = adapter
         it.layoutManager = lm
-        // setupBottomSheetInitialState(it)
+        setupBottomSheetInitialState(it)
     }
 
     override fun onDetachedFromWindow() {
